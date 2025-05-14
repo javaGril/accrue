c38e997db04d7e2d95152d0bc8722e70e05e59e3
jingling.fang <jingling.fang@lbk.one>
2025-05-14 16:19:25 +0800

    首次提交

---
+++ hooks.ts
@@ -0,0 +1,13 @@
+export const useMounted = () => {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+
+    return () => {
+      setMounted(false)
+    }
+  }, [])
+
+  return mounted
+}
